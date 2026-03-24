94000a3f45e9c1d30622977de4d2949e8dee9d6c
BAGGA-SHER-MD <kamran.md.bot@gmail.com>
2026-03-24 08:09:02 +0500

    Update command.js

--- command.js
+++ command.js
@@ -18,3 +18,20 @@ module.exports = {
     Module:cmd,
     commands,
 };
+   cmd.addCommand({
+    pattern: "setgdp",
+    desc: "To change Group DP",
+    category: "group",
+    filename: __filename
+},
+async(Void, citel, text) => {
+    if (!citel.isGroup) return citel.reply("Ye command sirf groups mein kaam karegi.");
+    if (!citel.quoted) return citel.reply("Kisi picture ko tag karke ye command likhen.");
+    
+    let mime = citel.quoted.mtype
+    if (!/image/.test(mime)) return citel.reply("Ye image nahi hai, please image tag karen.");
+
+    let img = await citel.quoted.download();
+    await Void.updateProfilePicture(citel.chat, img);
+    return citel.reply("Group DP successfully change ho gayi hai!");
+});
